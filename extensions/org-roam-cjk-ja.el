e38bb2a1006ef7a2975571b62b3448d5515305d0
Taro Sato <okomestudio@gmail.com>
2025-06-12 17:13:13 -0700

    Refactor to make components customizable

--- extensions/org-roam-cjk-ja.el
+++ extensions/org-roam-cjk-ja.el
@@ -4,9 +4,9 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/org-roam-cjk
-;; Version: 0.1.1
+;; Version: 0.2.1
 ;; Keywords: org-mode, org-roam, plug-in
-;; Package-Requires: ((emacs "30.1") (org "9.7") (org-roam "2.3.0"))
+;; Package-Requires: ((emacs "30.1") (org "9.7") (org-roam "2.3.0") (s "1.13.1"))
 ;;
 ;;; License:
 ;;
@@ -30,52 +30,78 @@
 ;;; Code:
 
 (require 'org-roam-cjk)
+(require 's)
+
+(defcustom org-roam-cjk-ja-zenkaku-parens '("（「『【［〈《〔｛"
+                                            "）」』】］〉》〕｝")
+  "Zenkaku parentheses, left-side and right-side.")
+
+(defcustom org-roam-cjk-ja-zenkaku-puncts "。、，・／；："
+  "Zenkaku punctuations.")
+
+(defcustom org-roam-cjk-ja-joshi
+  '(("かい" "かり" "けど" "けれど" "けれども"
+     "し" "ぜ" "ぞ"
+     "たり" "つつ" "ってば" "て" "ても" "ところで" "とも"
+     "な" "ので" "のに"
+     "ば"
+     "まま" "ものか" "ものの" "もん"
+     "わ")
+    ("か" "が" "かしら" "がてら" "から" "きり" "くらい" "ぐらい" "こそ"
+     "さ" "さえ" "しか" "ずつ"
+     "だけ" "だの" "で" "では" "でも" "と" "とは" "とも"
+     "ながら" "なぞ" "など" "なり" "なんぞ" "に" "ね" "の" "のみ"
+     "は" "ばかり" "へ" "ほど"
+     "まで" "も"
+     "や" "やら" "よ" "より"
+     "を"))
+  "Joshi (助詞) terms.
+Each can come on the left side (first element) or the right side (second
+element) of a target term.")
+
+(defcustom org-roam-cjk-ja-misc
+  '("家"                      ; （漫画）家山田花子
+    "スト"                    ; （ピアニ）スト山田花子
+    "ター"                    ; （イラストレー）ター山田花子
+    )
+  "Miscellaneous terms that can come before a target term (proper noun).")
 
 (setopt
  org-roam-cjk-unlinked-references-word-boundary-re
  (let* (;; Strict version:
         ;;
-        ;; Unlike English, Japanese words are not delimited by character
-        ;; word boundary. In this stricter version, we rely on "joshi"
-        ;; particles (助詞; ja.wikipedia.org/wiki/%E5%8A%A9%E8%A9%9E) as
-        ;; boundary indicator. This implicitly assumes titles (words to be
-        ;; delimited by the word boundary) to be "meishi" (名詞).
-
-        ;; The collection of joshi that comes on the right side of word:
-        (joshi_r '("か" "が" "かしら" "がてら" "から" "きり" "くらい" "ぐらい" "こそ"
-                   "さ" "さえ" "しか" "ずつ"
-                   "だけ" "だの" "で" "では" "でも" "と" "とは" "とも"
-                   "ながら" "なぞ" "など" "なり" "なんぞ" "に" "ね" "の" "のみ"
-                   "は" "ばかり" "へ" "ほど"
-                   "まで" "も"
-                   "や" "やら" "よ" "より"
-                   "を"))
-        ;; The collection of joshi that comes on the left side of word:
-        (joshi_l (append joshi_r
-                         '("かい" "かり" "けど" "けれど" "けれども"
-                           "し" "ぜ" "ぞ"
-                           "たり" "つつ" "ってば" "て" "ても" "ところで" "とも"
-                           "な" "ので" "のに"
-                           "ば"
-                           "まま" "ものか" "ものの" "もん"
-                           "わ")))
-        ;; These are not joshi, but nouns that can come before
-        ;; (proper) nouns for descriptions, e.g., "政治家田中角栄":
-        (joshi_l (append joshi_l
-                         '("家" "スト")))
+        ;; Unlike English, Japanese words are not delimited by
+        ;; character word boundary. In this stricter version, we use
+        ;; "joshi" particles (助詞) as boundary indicator. This
+        ;; implicitly assumes titles (words to be delimited by the
+        ;; word boundary) to be "meishi" (名詞/noun).
+        (l (append (cadr org-roam-cjk-ja-joshi)
+                   (car org-roam-cjk-ja-joshi)
+                   org-roam-cjk-ja-misc
+                   (string-split (nth 0 org-roam-cjk-ja-zenkaku-parens) "" t)
+                   (string-split (nth 1 org-roam-cjk-ja-zenkaku-parens) "" t)
+                   (string-split org-roam-cjk-ja-zenkaku-puncts "" t)))
+        (r (append (cadr org-roam-cjk-ja-joshi)
+                   (string-split (nth 1 org-roam-cjk-ja-zenkaku-parens) "" t)
+                   (string-split (nth 0 org-roam-cjk-ja-zenkaku-parens) "" t)
+                   (string-split org-roam-cjk-ja-zenkaku-puncts "" t)))
 
         (word-boundary-re-strict
          (concat "|(\\b%1$s\\b"
                  "|(?<="
-                 (s-join "|" joshi_l) ")%1$s(?=" (s-join "|" joshi_r) "))"))
+                 (s-join "|" (mapcar (lambda (it) (regexp-quote it)) l))
+                 ")%1$s(?="
+                 (s-join "|" (mapcar (lambda (it) (regexp-quote it)) r))
+                 "))"))
 
-        ;; Lenient version:
-        ;;
-        ;; Use any Japanese characters as word boundary:
+        ;; Lenient version (Use any Japanese characters as word
+        ;; boundary):
         (word-boundary-re-lenient
          (concat "|(\\b%1$s\\b"
                  "|(\\b%1$s\\b"
-                 "|(?<=[^\x20-\x7e\xff61-\xff9f])%1$s(?=[^\x20-\x7e\xff61-\xff9f]))")))
+                 "|(?<=[^\x20-\x7e\xff61-\xff9f])"
+                 "%1$s"
+                 "(?=[^\x20-\x7e\xff61-\xff9f]))")))
    word-boundary-re-strict))
 
 (provide 'org-roam-cjk-ja)
